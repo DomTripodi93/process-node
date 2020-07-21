bd468da88f10528716725b6d0c7276da52426d9d
DomTripodi93 <dtripodi93@gmail.com>
2020-07-21 13:04:05 -0400

    added MongoDb connection string using env variables

--- backend/main.js
+++ backend/main.js
@@ -3,13 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const env = require("./.env/env");
 const userRoutes = require("./routes/user");
+
 
 const app = express();
 
 mongoose
   .connect(
-    ""
+    "mongodb+srv://proc:" + 
+    env.dbpw +
+    "@proc.sfuco.mongodb.net/" +
+    env.dbName +
+    "?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database!");
