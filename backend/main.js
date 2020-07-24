59aa11e9124226449263657c86c7eb1e4034550b
DomTripodi93 <dtripodi93@gmail.com>
2020-07-24 12:11:40 -0400

    passing Department model to department routes

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,9 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const departmentRoutes = require("./routes/departmentRoutes")
+
+const Department = require("./models/departmentModel");
+const departmentRoutes = require("./routes/departmentRoutes")(Department);
 
 const env = require("./.env/env");
 const userRoutes = require("./routes/userRoutes");
