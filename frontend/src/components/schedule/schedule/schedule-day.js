308edd4e3d6f9d9ec46adcf87d75742438036e32
DomTripodi93 <dtripodi93@gmail.com>
2020-07-28 23:31:13 -0400

    abstracted schedule employee list from schedule day component

--- frontend/src/components/schedule/schedule/schedule-day.js
+++ frontend/src/components/schedule/schedule/schedule-day.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { deleteSchedule } from '../../../reducers/schedule/schedule/schedule.actions';
 import ScheduledTaskForm from './schedule-form';
 import ScheduleNone from './schedule-none';
+import ScheduleEmployeeList from './schedule-employee-list';
 
 
 
@@ -51,47 +52,17 @@ const ScheduleDay = props => {
             {props.scheduledTasks && props.scheduledTasks.length > 0 ?
                 <div>
                     {props.employeeId ?
-                        <div>
-                            <div className="grid-one-employee-button size-holder middle">
-                                <div className="grid-one-employee">
-                                    <div className="inner-border-left-header">
-                                        <h5 className="grid-header-text">Time</h5>
-                                    </div>
-                                    <div className="inner-border-left-header">
-                                        <h5 className="grid-header-text">Department</h5>
-                                    </div>
-                                    <div className="inner-border-left-header">
-                                        <h5 className="grid-header-text">Objective</h5>
-                                    </div>
-                                </div>
-                                <div className="inner-border-right-header"></div>
-                            </div>
-                            {props.scheduledTasks.map(scheduledTask => (
-                                <div
-                                    key={scheduledTask._id}
-                                    className="grid-one-employee-button size-holder middle">
-                                    <SingleScheduledTask
-                                        objectives={props.objectives}
-                                        scheduledTask={scheduledTask}
-                                        employeeMap={props.employeeMap}
-                                        employeeId={props.employeeId}
-                                        year={props.year}
-                                        month={props.month}
-                                        day={props.day}
-                                        className='sized30' />
-                                    <div className="grid50-colapse inner-border-right">
-                                        <CustomButton
-                                            buttonStyle="blue small"
-                                            action={() => { setEditMode(scheduledTask) }}
-                                            label="Edit" />
-                                        <CustomButton
-                                            buttonStyle="red small"
-                                            action={() => { handleDelete(scheduledTask) }}
-                                            label="Delete" />
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+                        <ScheduleEmployeeList
+                            scheduledTasks={props.scheduledTasks}
+                            objectives={props.objectives}
+                            employeeMap={props.employeeMap}
+                            employeeId={props.employeeId}
+                            year={props.year}
+                            month={props.month}
+                            day={props.day}
+                            handleEdit={setEditMode}
+                            handleDelete={handleDelete}
+                            />
                         :
                         <div>
                             <div className="grid-all-employees-button size-holder middle">
---
+++ frontend/src/components/schedule/schedule/schedule-employee-list.js
@@ -0,0 +1,51 @@
+import React from "react";
+import SingleScheduledTask from './single-scheduled-task';
+import CustomButton from '../../../shared/elements/button/custom-button.component';
+
+const ScheduleEmployeeList = props => {
+    return(
+        <div>
+            <div className="grid-one-employee-button size-holder middle">
+                <div className="grid-one-employee">
+                    <div className="inner-border-left-header">
+                        <h5 className="grid-header-text">Time</h5>
+                    </div>
+                    <div className="inner-border-left-header">
+                        <h5 className="grid-header-text">Department</h5>
+                    </div>
+                    <div className="inner-border-left-header">
+                        <h5 className="grid-header-text">Objective</h5>
+                    </div>
+                </div>
+                <div className="inner-border-right-header"></div>
+            </div>
+            {props.scheduledTasks.map(scheduledTask => (
+                <div
+                    key={scheduledTask._id}
+                    className="grid-one-employee-button size-holder middle">
+                    <SingleScheduledTask
+                        objectives={props.objectives}
+                        scheduledTask={scheduledTask}
+                        employeeMap={props.employeeMap}
+                        employeeId={props.employeeId}
+                        year={props.year}
+                        month={props.month}
+                        day={props.day}
+                        className='sized30' />
+                    <div className="grid50-colapse inner-border-right">
+                        <CustomButton
+                            buttonStyle="blue small"
+                            action={() => { props.handleEdit(scheduledTask) }}
+                            label="Edit" />
+                        <CustomButton
+                            buttonStyle="red small"
+                            action={() => { props.handleDelete(scheduledTask) }}
+                            label="Delete" />
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ScheduleEmployeeList;
