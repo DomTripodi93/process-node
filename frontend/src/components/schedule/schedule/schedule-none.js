a717c4b2c016d70330f0ed3bc6930214ee662e00
DomTripodi93 <dtripodi93@gmail.com>
2020-08-02 14:12:59 -0400

    updated conditional rendering inside of ScheduleNone

--- frontend/src/components/schedule/schedule/schedule-none.js
+++ frontend/src/components/schedule/schedule/schedule-none.js
@@ -2,31 +2,23 @@ import React from "react";
 
 const ScheduleNone = props => {
     return(
-        <div>
+        <div className="border centered">
             {props.employeeId ?
-                <div className="border centered">
-                    <h4 className="spaced">
-                        You currently don't have any scheduled tasks
-                        for {props.employeeMap[props.employeeId]} on {props.month}
-                        -{props.day}-{props.year}!
-                    </h4>
-                    <h4 className="spaced">
-                        Add some scheduled tasks using the button above to
-                        see them here.
-                    </h4>
-                </div>
+                <h4 className="spaced">
+                    You currently don't have any scheduled tasks
+                    for {props.employeeMap[props.employeeId]} on {props.month}
+                    -{props.day}-{props.year}!
+                </h4>
                 :
-                <div className="border centered">
-                    <h4 className="spaced">
-                        You currently don't have any scheduled tasks
-                        for {props.month}-{props.day}-{props.year}!
-                    </h4>
-                    <h4 className="spaced">
-                        Add some scheduled tasks using the button above to
-                        see them here.
-                    </h4>
-                </div>
+                <h4 className="spaced">
+                    You currently don't have any scheduled tasks
+                    for {props.month}-{props.day}-{props.year}!
+                </h4>
             }
+            <h4 className="spaced">
+                Add some scheduled tasks using the button above to
+                see them here.
+            </h4>
         </div>
     )
 }
