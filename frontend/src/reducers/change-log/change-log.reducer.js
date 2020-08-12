9c8448e3ab1cd37a0d2ee77be7a101471635d805
Dom <dtripodi93@gmail.com>
2020-08-12 19:53:17 -0400

    fixed change log reducer data storage of paged data

--- frontend/src/reducers/change-log/change-log.reducer.js
+++ frontend/src/reducers/change-log/change-log.reducer.js
@@ -9,7 +9,11 @@ const ChangeLogReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ChangeLogActionTypes.SET_CHANGES:
             let changeHold = { ...state.changes }
-            changeHold[action.model] = { [action.page]: action.payload.data };
+            if (changeHold[action.model]){
+                changeHold[action.model][action.page] = action.payload.data;
+            } else {
+                changeHold[action.model] = { [action.page]: action.payload.data };
+            }
             let moreResultHold = { ...state.moreResults };
             if (action.payload.data.length === 10) {
                 moreResultHold[action.model] = true;
